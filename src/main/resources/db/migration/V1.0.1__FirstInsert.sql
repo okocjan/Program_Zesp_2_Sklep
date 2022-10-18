7fdf509088ca35864fcc6749b6edc71118268471
Oskar <kocjan.oskar@gmail.com>
2022-10-18 18:16:58 +0200

    first db insert

---
+++ src/main/resources/db/migration/V1.0.1__FirstInsert.sql
@@ -0,0 +1,2 @@
+INSERT INTO storedb.product VALUES (1, 'BOSCH My Friend', 20, 114.70, 1);
+INSERT INTO storedb.product_picture VALUES (1, 'https://media.os.fressnapf.com/products-v2/f/7/0/8/f708c03ad682b65f64d6e7ccef78d1285ad6de14_cd38213dbef9114b0854eee165c051d6536f6613.jpg?t=prod_s&f=webp', 1);
